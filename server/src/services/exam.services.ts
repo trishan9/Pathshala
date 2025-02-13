07b25c29b1dd21226429c8d2cd502aa6d2d50780
Trishan Wagle <mailtotrishan@gmail.com>
2025-02-13 19:37:40 +0545

    feat: exam service function

---
+++ server/src/services/exam.services.ts
@@ -0,0 +1,51 @@
+import client from "@/db";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { Prisma } from "@prisma/client";
+
+export interface GetExamParams {
+  page?: number;
+  classId?: number;
+  teacherId?: string;
+  search?: string;
+}
+
+export const getAllExams = async (params: GetExamParams) => {
+  const { page = 1, classId, teacherId, search } = params;
+
+  const whereClause: Prisma.ExamWhereInput = {
+    ...(classId && {
+      lesson: { classId: +classId },
+    }),
+    lesson: { teacherId },
+    ...(search && {
+      lesson: {
+        subject: {
+          name: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+      },
+    }),
+  };
+
+  const [exams, examsCount] = await client.$transaction([
+    client.exam.findMany({
+      where: whereClause,
+      include: {
+        lesson: {
+          select: {
+            subject: { select: { name: true } },
+            teacher: { select: { name: true } },
+            class: { select: { name: true } },
+          },
+        },
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (page - 1),
+    }),
+    client.exam.count({ where: whereClause }),
+  ]);
+
+  return { exams, examsCount };
+};
