b53449dc2a90e6021dc930fc803518f2c976deb4
Trishan Wagle <mailtotrishan@gmail.com>
2025-02-13 12:37:23 +0545

    feat: add pagination and search functionality

--- client/src/components/tables/TableSearch.tsx
+++ client/src/components/tables/TableSearch.tsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "@tanstack/react-router";
 import { Search } from "lucide-react";
 
 const TableSearch = () => {
+  const navigate = useNavigate({ from: "/list/teachers" });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const query = (e.currentTarget[0] as HTMLInputElement).value;
+    navigate({ search: (prev) => ({ ...prev, search: query }) });
+  };
+
   return (
-    <div className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+    <form
+      onSubmit={handleSubmit}
+      className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2"
+    >
       <Search className="w-4 h-4" />
 
       <input
@@ -10,7 +22,7 @@ const TableSearch = () => {
         placeholder="Search..."
         className="w-[200px] p-2 bg-transparent outline-none"
       />
-    </div>
+    </form>
   );
 };
 
