540d437a3bcf57129763438c24ff58c5d401f4a0
Trishan Wagle <mailtotrishan@gmail.com>
2025-02-13 18:36:38 +0545

    feat: service function for lessons

---
+++ server/src/services/lesson.services.ts
@@ -0,0 +1,39 @@
+import client from "@/db";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { Prisma } from "@prisma/client";
+
+export interface GetLessonParams {
+  page?: number;
+  teacherId?: string;
+  search?: string;
+}
+
+export const getAllLessons = async (params: GetLessonParams) => {
+  const { page = 1, teacherId, search } = params;
+
+  const whereClause: Prisma.LessonWhereInput = {
+    teacherId,
+    ...(search && {
+      name: {
+        contains: search,
+        mode: "insensitive",
+      },
+    }),
+  };
+
+  const [lessons, lessonsCount] = await client.$transaction([
+    client.lesson.findMany({
+      where: whereClause,
+      include: {
+        subject: { select: { name: true } },
+        class: { select: { name: true } },
+        teacher: { select: { name: true } },
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (page - 1),
+    }),
+    client.lesson.count({ where: whereClause }),
+  ]);
+
+  return { lessons, lessonsCount };
+};
