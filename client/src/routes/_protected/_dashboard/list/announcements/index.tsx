240d5c8330d122c193cde32cec39e8fcb3e2122d
Trishan Wagle <mailtotrishan@gmail.com>
2025-02-14 21:13:38 +0545

    feat: fetched announcements in frontend

--- client/src/routes/_protected/_dashboard/list/announcements/index.tsx
+++ client/src/routes/_protected/_dashboard/list/announcements/index.tsx
@@ -1,68 +1,87 @@
-import { createFileRoute } from '@tanstack/react-router'
-import FormModal from '@/components/forms/FormModal'
-import Pagination from '@/components/tables/Pagination'
-import Table from '@/components/tables/Table'
-import TableSearch from '@/components/tables/TableSearch'
-import { announcementsData, role } from '@/lib/data'
+import { createFileRoute } from "@tanstack/react-router";
+import FormModal from "@/components/forms/FormModal";
+import Pagination from "@/components/tables/Pagination";
+import Table from "@/components/tables/Table";
+import TableSearch from "@/components/tables/TableSearch";
+import { role } from "@/lib/data";
+import { z } from "zod";
+import { Class } from "../classes";
+import { useGetAnnouncements } from "@/hooks/useAnnouncements";
+import { PageLoader } from "@/components/PageLoader";
+
+const getAllAnnouncementsQuerySchema = z.object({
+  page: z.number().optional(),
+  search: z.string().optional(),
+});
 
 export const Route = createFileRoute(
-  '/_protected/_dashboard/list/announcements/',
+  "/_protected/_dashboard/list/announcements/",
 )({
   component: RouteComponent,
-})
+  validateSearch: getAllAnnouncementsQuerySchema,
+});
 
 function RouteComponent() {
-  return <AnnouncementListPage />
+  return <AnnouncementListPage />;
 }
 
 type Announcement = {
-  id: number
-  title: string
-  class: string
-  date: string
-}
+  id: number;
+  title: string;
+  description: string;
+  class: Class;
+  date: Date;
+};
 
 const columns = [
   {
-    header: 'Title',
-    accessor: 'title',
+    header: "Title",
+    accessor: "title",
   },
   {
-    header: 'Class',
-    accessor: 'class',
+    header: "Class",
+    accessor: "class",
   },
   {
-    header: 'Date',
-    accessor: 'date',
-    className: 'hidden md:table-cell',
+    header: "Date",
+    accessor: "date",
+    className: "hidden md:table-cell",
   },
   {
-    header: 'Actions',
-    accessor: 'action',
+    header: "Actions",
+    accessor: "action",
   },
-]
+];
+
+const renderRow = (item: Announcement) => (
+  <tr
+    key={item.id}
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+  >
+    <td className="flex items-center gap-4 p-4">{item.title}</td>
+    <td>{item.class.name}</td>
+    <td className="hidden md:table-cell">
+      {new Intl.DateTimeFormat("en-US").format(new Date(item.date))}
+    </td>
+    <td>
+      <div className="flex items-center gap-2">
+        {role === "admin" && (
+          <>
+            <FormModal table="announcement" type="update" data={item} />
+            <FormModal table="announcement" type="delete" id={item.id} />
+          </>
+        )}
+      </div>
+    </td>
+  </tr>
+);
 
 const AnnouncementListPage = () => {
-  const renderRow = (item: Announcement) => (
-    <tr
-      key={item.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
-    >
-      <td className="flex items-center gap-4 p-4">{item.title}</td>
-      <td>{item.class}</td>
-      <td className="hidden md:table-cell">{item.date}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          {role === 'admin' && (
-            <>
-              <FormModal table="announcement" type="update" data={item} />
-              <FormModal table="announcement" type="delete" id={item.id} />
-            </>
-          )}
-        </div>
-      </td>
-    </tr>
-  )
+  const params = Route.useSearch();
+  const { data, isLoading } = useGetAnnouncements({
+    page: params.page,
+    search: params.search,
+  });
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 border">
@@ -80,18 +99,32 @@ const AnnouncementListPage = () => {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <img src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {role === 'admin' && (
+            {role === "admin" && (
               <FormModal table="announcement" type="create" />
             )}
           </div>
         </div>
       </div>
-      {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={announcementsData} />
-      {/* PAGINATION */}
-      <Pagination />
-    </div>
-  )
-}
 
-export default AnnouncementListPage
+      {isLoading && <PageLoader />}
+
+      {data && (
+        <>
+          {/* LIST */}
+          <Table
+            columns={columns}
+            renderRow={renderRow}
+            data={data?.announcements}
+          />
+          {/* PAGINATION */}
+          <Pagination
+            page={params?.page || 1}
+            count={data?.announcementsCount}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default AnnouncementListPage;
