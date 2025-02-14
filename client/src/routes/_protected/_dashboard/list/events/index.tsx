a05af512efbe9ba0e475a4d5e69081d4ed4c1812
Trishan Wagle <mailtotrishan@gmail.com>
2025-02-14 11:07:44 +0545

    feat: fetched class events with search functionality

--- client/src/routes/_protected/_dashboard/list/events/index.tsx
+++ client/src/routes/_protected/_dashboard/list/events/index.tsx
@@ -1,80 +1,110 @@
-import { createFileRoute } from '@tanstack/react-router'
-import FormModal from '@/components/forms/FormModal'
-import Pagination from '@/components/tables/Pagination'
-import Table from '@/components/tables/Table'
-import TableSearch from '@/components/tables/TableSearch'
-import { eventsData, role } from '@/lib/data'
+import { createFileRoute } from "@tanstack/react-router";
+import FormModal from "@/components/forms/FormModal";
+import Pagination from "@/components/tables/Pagination";
+import Table from "@/components/tables/Table";
+import TableSearch from "@/components/tables/TableSearch";
+import { role } from "@/lib/data";
+import { z } from "zod";
+import { Class } from "../classes";
+import { useGetEvents } from "@/hooks/useEvents";
+import { PageLoader } from "@/components/PageLoader";
 
-export const Route = createFileRoute('/_protected/_dashboard/list/events/')({
+const getAllEventsQuerySchema = z.object({
+  page: z.number().optional(),
+  search: z.string().optional(),
+});
+
+export const Route = createFileRoute("/_protected/_dashboard/list/events/")({
   component: RouteComponent,
-})
+  validateSearch: getAllEventsQuerySchema,
+});
 
 function RouteComponent() {
-  return <EventListPage />
+  return <EventListPage />;
 }
 
 type Event = {
-  id: number
-  title: string
-  class: string
-  date: string
-  startTime: string
-  endTime: string
-}
+  id: number;
+  title: string;
+  description: string;
+  class: Class;
+  startTime: Date;
+  endTime: Date;
+};
 
 const columns = [
   {
-    header: 'Title',
-    accessor: 'title',
+    header: "Title",
+    accessor: "title",
   },
   {
-    header: 'Class',
-    accessor: 'class',
+    header: "Class",
+    accessor: "class",
   },
   {
-    header: 'Date',
-    accessor: 'date',
-    className: 'hidden md:table-cell',
+    header: "Date",
+    accessor: "date",
+    className: "hidden md:table-cell",
   },
   {
-    header: 'Start Time',
-    accessor: 'startTime',
-    className: 'hidden md:table-cell',
+    header: "Start Time",
+    accessor: "startTime",
+    className: "hidden md:table-cell",
   },
   {
-    header: 'End Time',
-    accessor: 'endTime',
-    className: 'hidden md:table-cell',
+    header: "End Time",
+    accessor: "endTime",
+    className: "hidden md:table-cell",
   },
   {
-    header: 'Actions',
-    accessor: 'action',
+    header: "Actions",
+    accessor: "action",
   },
-]
+];
+
+const renderRow = (item: Event) => (
+  <tr
+    key={item.id}
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+  >
+    <td className="flex items-center gap-4 p-4">{item.title}</td>
+    <td>{item.class.name || "-"}</td>
+    <td className="hidden md:table-cell">
+      {new Intl.DateTimeFormat("en-US").format(new Date(item.startTime))}
+    </td>
+    <td className="hidden md:table-cell">
+      {new Date(item.startTime).toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+      })}
+    </td>
+    <td className="hidden md:table-cell">
+      {new Date(item.endTime).toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+      })}
+    </td>
+    <td>
+      <div className="flex items-center gap-2">
+        {role === "admin" && (
+          <>
+            <FormModal table="event" type="update" data={item} />
+            <FormModal table="event" type="delete" id={item.id} />
+          </>
+        )}
+      </div>
+    </td>
+  </tr>
+);
 
 const EventListPage = () => {
-  const renderRow = (item: Event) => (
-    <tr
-      key={item.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
-    >
-      <td className="flex items-center gap-4 p-4">{item.title}</td>
-      <td>{item.class}</td>
-      <td className="hidden md:table-cell">{item.date}</td>
-      <td className="hidden md:table-cell">{item.startTime}</td>
-      <td className="hidden md:table-cell">{item.endTime}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          {role === 'admin' && (
-            <>
-              <FormModal table="event" type="update" data={item} />
-              <FormModal table="event" type="delete" id={item.id} />
-            </>
-          )}
-        </div>
-      </td>
-    </tr>
-  )
+  const params = Route.useSearch();
+  const { data, isLoading } = useGetEvents({
+    page: params.page,
+    search: params.search,
+  });
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 border">
@@ -90,16 +120,23 @@ const EventListPage = () => {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <img src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {role === 'admin' && <FormModal table="event" type="create" />}
+            {role === "admin" && <FormModal table="event" type="create" />}
           </div>
         </div>
       </div>
-      {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={eventsData} />
-      {/* PAGINATION */}
-      <Pagination />
-    </div>
-  )
-}
 
-export default EventListPage
+      {isLoading && <PageLoader />}
+
+      {data && (
+        <>
+          {/* LIST */}
+          <Table columns={columns} renderRow={renderRow} data={data?.events} />
+          {/* PAGINATION */}
+          <Pagination page={params?.page || 1} count={data?.eventsCount} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default EventListPage;
