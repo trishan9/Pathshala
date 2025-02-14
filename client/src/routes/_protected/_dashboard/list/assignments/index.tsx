cfc94dae57582b6bbdf14f21482f3cc3d67d5250
Trishan Wagle <mailtotrishan@gmail.com>
2025-02-14 09:21:36 +0545

    feat: fetched, filter, and search assignments client

--- client/src/routes/_protected/_dashboard/list/assignments/index.tsx
+++ client/src/routes/_protected/_dashboard/list/assignments/index.tsx
@@ -1,75 +1,95 @@
-import { createFileRoute } from '@tanstack/react-router'
-import FormModal from '@/components/forms/FormModal'
-import Pagination from '@/components/tables/Pagination'
-import Table from '@/components/tables/Table'
-import TableSearch from '@/components/tables/TableSearch'
-import { assignmentsData, role } from '@/lib/data'
+import { createFileRoute } from "@tanstack/react-router";
+import FormModal from "@/components/forms/FormModal";
+import Pagination from "@/components/tables/Pagination";
+import Table from "@/components/tables/Table";
+import TableSearch from "@/components/tables/TableSearch";
+import { role } from "@/lib/data";
+import { z } from "zod";
+import { Lesson } from "../lessons";
+import { useGetAssignments } from "@/hooks/useAssignments";
+import { PageLoader } from "@/components/PageLoader";
+
+const getAllAssignmentsQuerySchema = z.object({
+  page: z.number().optional(),
+  classId: z.number().optional(),
+  teacherId: z.string().optional(),
+  search: z.string().optional(),
+});
 
 export const Route = createFileRoute(
-  '/_protected/_dashboard/list/assignments/',
+  "/_protected/_dashboard/list/assignments/",
 )({
   component: RouteComponent,
-})
+  validateSearch: getAllAssignmentsQuerySchema,
+});
 
 function RouteComponent() {
-  return <AssignmentListPage />
+  return <AssignmentListPage />;
 }
 
 type Assignment = {
-  id: number
-  subject: string
-  class: string
-  teacher: string
-  dueDate: string
-}
+  id: number;
+  dueDate: Date;
+  lesson: Lesson;
+};
 
 const columns = [
   {
-    header: 'Subject Name',
-    accessor: 'name',
+    header: "Subject Name",
+    accessor: "name",
   },
   {
-    header: 'Class',
-    accessor: 'class',
+    header: "Class",
+    accessor: "class",
   },
   {
-    header: 'Teacher',
-    accessor: 'teacher',
-    className: 'hidden md:table-cell',
+    header: "Teacher",
+    accessor: "teacher",
+    className: "hidden md:table-cell",
   },
   {
-    header: 'Due Date',
-    accessor: 'dueDate',
-    className: 'hidden md:table-cell',
+    header: "Due Date",
+    accessor: "dueDate",
+    className: "hidden md:table-cell",
   },
   {
-    header: 'Actions',
-    accessor: 'action',
+    header: "Actions",
+    accessor: "action",
   },
-]
+];
+
+const renderRow = (item: Assignment) => (
+  <tr
+    key={item.id}
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+  >
+    <td className="flex items-center gap-4 p-4">{item.lesson.subject.name}</td>
+    <td>{item.lesson.class.name}</td>
+    <td className="hidden md:table-cell">{item.lesson.teacher.name}</td>
+    <td className="hidden md:table-cell">
+      {new Intl.DateTimeFormat("en-US").format(new Date(item.dueDate))}
+    </td>
+    <td>
+      <div className="flex items-center gap-2">
+        {(role === "admin" || role === "teacher") && (
+          <>
+            <FormModal table="assignment" type="update" data={item} />
+            <FormModal table="assignment" type="delete" id={item.id} />
+          </>
+        )}
+      </div>
+    </td>
+  </tr>
+);
 
 const AssignmentListPage = () => {
-  const renderRow = (item: Assignment) => (
-    <tr
-      key={item.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
-    >
-      <td className="flex items-center gap-4 p-4">{item.subject}</td>
-      <td>{item.class}</td>
-      <td className="hidden md:table-cell">{item.teacher}</td>
-      <td className="hidden md:table-cell">{item.dueDate}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          {(role === 'admin' || role === 'teacher') && (
-            <>
-              <FormModal table="assignment" type="update" data={item} />
-              <FormModal table="assignment" type="delete" id={item.id} />
-            </>
-          )}
-        </div>
-      </td>
-    </tr>
-  )
+  const params = Route.useSearch();
+  const { data, isLoading } = useGetAssignments({
+    page: params.page,
+    classId: params.classId,
+    teacherId: params.teacherId,
+    search: params.search,
+  });
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 border">
@@ -87,18 +107,29 @@ const AssignmentListPage = () => {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <img src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {(role === 'admin' || role === 'teacher') && (
+            {(role === "admin" || role === "teacher") && (
               <FormModal table="assignment" type="create" />
             )}
           </div>
         </div>
       </div>
-      {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={assignmentsData} />
-      {/* PAGINATION */}
-      <Pagination />
-    </div>
-  )
-}
 
-export default AssignmentListPage
+      {isLoading && <PageLoader />}
+
+      {data && (
+        <>
+          {/* LIST */}
+          <Table
+            columns={columns}
+            renderRow={renderRow}
+            data={data?.assignments}
+          />
+          {/* PAGINATION */}
+          <Pagination page={params?.page || 1} count={data?.assignmentsCount} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default AssignmentListPage;
