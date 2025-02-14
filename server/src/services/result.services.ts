3cce1d69185e03d7e401091bda7c79a99b3baf73
Trishan Wagle <mailtotrishan@gmail.com>
2025-02-14 09:37:33 +0545

    feat: service function for exam and assignment results

---
+++ server/src/services/result.services.ts
@@ -0,0 +1,91 @@
+import client from "@/db";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { Prisma } from "@prisma/client";
+
+export interface GetResultParams {
+  page?: number;
+  studentId?: string;
+  search?: string;
+}
+
+export const getAllResults = async (params: GetResultParams) => {
+  const { page = 1, studentId, search } = params;
+
+  const whereClause: Prisma.ResultWhereInput = {
+    ...(studentId && {
+      studentId,
+    }),
+    ...(search && {
+      OR: [
+        {
+          exam: {
+            title: {
+              contains: search,
+              mode: "insensitive",
+            },
+          },
+        },
+        {
+          student: {
+            name: {
+              contains: search,
+              mode: "insensitive",
+            },
+          },
+        },
+      ],
+    }),
+  };
+
+  const [resultsRes, resultsCount] = await client.$transaction([
+    client.result.findMany({
+      where: whereClause,
+      include: {
+        student: { select: { name: true } },
+        exam: {
+          include: {
+            lesson: {
+              select: {
+                class: { select: { name: true } },
+                teacher: { select: { name: true } },
+              },
+            },
+          },
+        },
+        assignment: {
+          include: {
+            lesson: {
+              select: {
+                class: { select: { name: true } },
+                teacher: { select: { name: true } },
+              },
+            },
+          },
+        },
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (page - 1),
+    }),
+    client.result.count({ where: whereClause }),
+  ]);
+
+  const results = resultsRes.map((item) => {
+    const assessment = item.exam || item.assignment;
+
+    if (!assessment) return null;
+
+    const isExam = "startTime" in assessment;
+
+    return {
+      id: item.id,
+      title: assessment.title,
+      studentName: item.student.name,
+      teacherName: assessment.lesson.teacher.name,
+      score: item.score,
+      className: assessment.lesson.class.name,
+      startTime: isExam ? assessment.startTime : assessment.startDate,
+    };
+  });
+
+  return { results, resultsCount };
+};
