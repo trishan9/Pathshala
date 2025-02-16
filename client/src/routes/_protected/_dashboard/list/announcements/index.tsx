11d2ff034ef766dc53456acaf8a6a2efbdfc718d
Trishan Wagle <mailtotrishan@gmail.com>
2025-02-16 11:15:24 +0545

    fix: role-based permissions for announcements

--- client/src/routes/_protected/_dashboard/list/announcements/index.tsx
+++ client/src/routes/_protected/_dashboard/list/announcements/index.tsx
@@ -3,11 +3,11 @@ import FormModal from "@/components/forms/FormModal";
 import Pagination from "@/components/tables/Pagination";
 import Table from "@/components/tables/Table";
 import TableSearch from "@/components/tables/TableSearch";
-import { role } from "@/lib/data";
 import { z } from "zod";
 import { Class } from "../../_staff/list/classes";
 import { useGetAnnouncements } from "@/hooks/useAnnouncements";
 import { PageLoader } from "@/components/PageLoader";
+import { useAuthStore } from "@/stores/authStore";
 
 const getAllAnnouncementsQuerySchema = z.object({
   page: z.number().optional(),
@@ -33,55 +33,60 @@ type Announcement = {
   date: Date;
 };
 
-const columns = [
-  {
-    header: "Title",
-    accessor: "title",
-  },
-  {
-    header: "Class",
-    accessor: "class",
-  },
-  {
-    header: "Date",
-    accessor: "date",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Actions",
-    accessor: "action",
-  },
-];
-
-const renderRow = (item: Announcement) => (
-  <tr
-    key={item.id}
-    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
-  >
-    <td className="flex items-center gap-4 p-4">{item.title}</td>
-    <td>{item.class.name}</td>
-    <td className="hidden md:table-cell">
-      {new Intl.DateTimeFormat("en-US").format(new Date(item.date))}
-    </td>
-    <td>
-      <div className="flex items-center gap-2">
-        {role === "admin" && (
-          <>
-            <FormModal table="announcement" type="update" data={item} />
-            <FormModal table="announcement" type="delete" id={item.id} />
-          </>
-        )}
-      </div>
-    </td>
-  </tr>
-);
-
 const AnnouncementListPage = () => {
   const params = Route.useSearch();
   const { data, isLoading } = useGetAnnouncements({
     page: params.page,
     search: params.search,
   });
+  const role = useAuthStore((state) => state.user)?.role;
+
+  const columns = [
+    {
+      header: "Title",
+      accessor: "title",
+    },
+    {
+      header: "Class",
+      accessor: "class",
+    },
+    {
+      header: "Date",
+      accessor: "date",
+      className: "hidden md:table-cell",
+    },
+    ...(role === "admin"
+      ? [
+          {
+            header: "Actions",
+            accessor: "action",
+          },
+        ]
+      : []),
+  ];
+
+  const renderRow = (item: Announcement) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">{item.title}</td>
+      <td>{item.class.name}</td>
+      <td className="hidden md:table-cell">
+        {new Intl.DateTimeFormat("en-US").format(new Date(item.date))}
+      </td>
+      <td>
+        <div className="flex items-center gap-2">
+          {role === "admin" && (
+            <>
+              <FormModal table="announcement" type="update" data={item} />
+              <FormModal table="announcement" type="delete" id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 border">
